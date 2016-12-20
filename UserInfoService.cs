029ecf97b89321fa55e7b08e440b1cf9fda0dc8f
zhanggang99 <zhanggang@outlook.com>
2016-12-20 14:00:31 +0800

    Add files via upload

---
+++ UserInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Model;
+namespace BLL
+{
+    public class UserInfoService
+    {
+        DAL.UserInfoDal userInfoDal = new UserInfoDal();
+        public bool IsExist(string email,string password)
+        {
+            return userInfoDal.IsExist(email,password);
+        }
+    }
+}
