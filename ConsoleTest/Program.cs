19cab4ee154802c066d698fb48bfea30c4b82504
zhanggang99 <zhanggang@outlook.com>
2016-12-28 20:14:58 +0800

    一个测试.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -3,14 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleTest
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("string≤‚ ‘");
+            ThreadTest t1 = new ThreadTest();
+            ThreadTest tt1 = new ThreadTest();
+            Thread td11 = new Thread(t1.Run1);
+            td11.Start();
+            Thread td21 = new Thread(t1.Run2);
+            //Thread td21 = new Thread(t1.Run1);
+            td21.Start();
 
+            Console.WriteLine("wait 1S ");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("stringbuild≤‚ ‘");
+            ThreadTest t = new ThreadTest();
+            Thread td1 = new Thread(t.Run1);
+            td1.Start();
+            Thread td2 = new Thread(t.Run2);
+            td2.Start();
+            Console.ReadKey();
+
+            //Console.WriteLine("stringbuild≤‚ ‘");
+            //ThreadTest t = new ThreadTest();
+            ////t.Test();
+            //ThreadTest tt = new ThreadTest();
+            //Thread td1 = new Thread(t.Run1);
+            //td1.Start();
+            //Thread td2 = new Thread(tt.Run2);
+            //td2.Start();
+            //Console.ReadKey();
+
+            //Console.WriteLine("string≤‚ ‘");
+            //ThreadTest t1 = new ThreadTest();
+            //ThreadTest tt1 = new ThreadTest();
+            //Thread td11 = new Thread(t1.Run1);
+            //td11.Start();
+            //Thread td21 = new Thread(tt1.Run2);
+            //td21.Start();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ThreadTest
+    {
+        private StringBuilder sb = new StringBuilder();
+        private string sb2 = string.Empty;
+        public void Test()
+        {
+            Thread td1 = new Thread(Run1);
+            td1.Start();
+            Thread td2 = new Thread(Run2);
+            td2.Start();
+
+
+        }
+        public void Run1()
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                sb.Append("aaaa").Append(Thread.CurrentThread.ManagedThreadId);
+                //sb += "aaaa";
+                Console.WriteLine(sb.ToString());
+            }
+        }
+        public void Run11()
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                sb2 += "aaaa" + Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+        public void Run2()
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                sb.Append("bbbb").Append(Thread.CurrentThread.ManagedThreadId);
+                //sb += "bbbb";
+                Console.WriteLine(sb.ToString());
+            }
+        }
+        public void Run22()
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                sb2 += "bbbb" + Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine(sb.ToString());
+            }
         }
     }
 }
