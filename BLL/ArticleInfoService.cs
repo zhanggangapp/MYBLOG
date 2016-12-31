859ffddd2579e6ad5b4cae88bd5a590f5b0a5932
zhanggang99 <zhanggang@outlook.com>
2016-12-31 19:15:47 +0800

    修正RSS数据来源

--- BLL/ArticleInfoService.cs
+++ BLL/ArticleInfoService.cs
@@ -5,13 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Model;
 using DAL;
+using System.Web;
+
 namespace BLL
 {
     public class ArticleInfoService
     {
+
         DAL.IArticleInfoDal articleInfoDal = new ArticleInfoSQLDal();
         public List<ArticleEntity> GetArticleList(DateTime beginTime)
         {
+            int DataType = Convert.ToInt32(HttpContext.Current.Application["DataType"]);
+            switch (DataType)
+            {
+                case 0:
+                    articleInfoDal = new ArticleInfoSQLDal();
+                    break;
+                case 2:
+                    articleInfoDal = new ArticleInfoESDal();
+                    break;
+                default:
+                    articleInfoDal = new ArticleInfoSQLDal();
+                    break;
+            }
             List<ArticleEntity> list = articleInfoDal.GetArticleList(beginTime);
             return list;
         }
