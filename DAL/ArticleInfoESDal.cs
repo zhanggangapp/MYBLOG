74cef04df950b76994ed25bbacd71c3f6e44291b
zhanggang99 <zhanggang@outlook.com>
2016-12-31 15:27:34 +0800

    rss修正

--- DAL/ArticleInfoESDal.cs
+++ DAL/ArticleInfoESDal.cs
@@ -12,26 +12,32 @@ namespace DAL
         public List<ArticleEntity> GetArticleList(DateTime beginTime)
         {
             //string sql = "SELECT BlogId,Title,CreatedTime,Content FROM dbo.BlogInfo WHERE CreatedTime>@beginTime ORDER BY BlogId DESC";
-            //var response = ESHelper.client.Search<BlogInfo>(s=>s.Query(q=>q.Range(r=>r.Field(f=>f.CreatedTime))).Sort(ss=>ss.Descending(p=>p.BlogId)));
+            var response = ESHelper.client.Search<BlogInfo>(s=>s.Query(q=>q.Bool(b=>b.Should(sd=>sd.TermRange(tr=>tr.GreaterThan(System.DateTime.Now.AddDays
+                (-30).ToString("yyyyMMddhhmmss")).Field(f=>f.BlogId))))).Sort(ss=>ss.Descending(p=>p.BlogId)));
 
-            //SqlParameter[] pars = {
-            //   new SqlParameter("@beginTime",System.Data.SqlDbType.DateTime)
-            //};
-            //pars[0].Value = beginTime;
-            //DataTable da = SqlHelper.ExecuteDataTable(sql, CommandType.Text, pars);
-            //List<ArticleEntity> list = null;
-            //if (da.Rows.Count > 0)
-            //{
-            //    list = new List<ArticleEntity>();
-            //    ArticleEntity articleInfo = null;
-            //    foreach (DataRow row in da.Rows)
-            //    {
-            //        articleInfo = new ArticleEntity();
-            //        LoadArticleEntity(row, articleInfo);
-            //        list.Add(articleInfo);
-            //    }
-            //}
+            List<ArticleEntity> list = null;
+            if (response.Documents.Count>0)
+            {
+                list = new List<ArticleEntity>();
+                ArticleEntity articleInfo = null;
+                foreach (BlogInfo documment in response.Documents)
+                {
+                    articleInfo = new ArticleEntity();
+                    LoadArticleEntity(documment, articleInfo);
+                    list.Add(articleInfo);
+                }
+            }
             return list;
         }
+        private void LoadArticleEntity(BlogInfo bloginfo, ArticleEntity articleInfo)
+        {
+            articleInfo.BlogId = bloginfo.BlogId;
+            articleInfo.Title = bloginfo.Title;
+            articleInfo.PostTime = bloginfo.CreatedTime;
+        }
     }
+   
+
 }
+
+//ArticleEntity
