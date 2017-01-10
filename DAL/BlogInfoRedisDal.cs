974b1f9f60e9301f4cf637786c95ce2247a9bd08
zhanggang99 <zhanggang@outlook.com>
2017-01-10 21:48:21 +0800

    准备支持redit存储

---
+++ DAL/BlogInfoRedisDal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    class BlogInfoRedisDal : IBlogInfoDal
+    {
+        public int AddBlogInfo(string title, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteBlogInfo(long blogid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EditBlogInfo(long blogid, string title, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogInfo GetBlogById(long blogid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBlogCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBlogListByKeyWordCount(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogInfo> GetBlogListByPage(int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogInfo> GetBlogTitleListByKeyWordPage(int start, int end, string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogInfo> GetBlogTitleListByPage(int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
